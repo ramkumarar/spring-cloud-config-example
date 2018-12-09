bd0fb4cf140bd87df2983b25624eaf31a4ba41d1
ram <ramkumara.ar@gmail.com>
2018-12-09 23:25:45 +0530

    initial commit

---
+++ src/main/java/com/springbootdev/springcloud/examples/springcloudconfigserver/ActuatorSecurity.java
@@ -0,0 +1,22 @@
+package com.springbootdev.springcloud.examples.springcloudconfigserver;
+
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class ActuatorSecurity extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+                 http.csrf().disable()
+                         .httpBasic().and()
+                         .authorizeRequests()
+                         .requestMatchers(EndpointRequest.to("health","info")).permitAll()
+                         .antMatchers("/**").authenticated();
+
+    }
+
+
+}
+
